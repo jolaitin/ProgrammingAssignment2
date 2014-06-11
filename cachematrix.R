7a0b55c4d839afb2df043cfca98c7bb1d031958d
Jouni <Admin@124x35x247x208.ap124.ftth.ucom.ne.jp>
2014-06-11 23:02:06 +0900

    First real commit?
    Yeah… Took time off to do other assignments and now i can’t remember
    what works and what doesn’t…

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,54 @@
 ##
-##  This is based on the forked copy of the R Programminc course repo
+##  This is based on a forked copy of the R Programminc course repo
 ##
-##
-## Put comments here that give an overall description of what your
-## functions do
+##  makeCacheMatrix creates a cached matrix, which can be used multiple times
+## 
+##  cacheSolve solves the inverse matrix
+## 
+
+
+
+## CAN'T REMEMBER FROM LAST TIME ANYMORE, CHECK WHAT WORKS OR NOT!!!
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  ##initialize
+  inv<-NULL
+  
+  set <- function (y){
+        x<<- y
+        inv <<- NULL
+      }
+  get <- function () x
+  setInverse <- function (inv_) inv<<-inv_
+  getInverse <- function () inv
+  list(set = set, get=get, setInverse = setInverse, getInverse=getInverse)
+  
+  
 }
 
 
-## Write a short comment describing this function
-
+## Return a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  inverse<- x$getInverse()
+  ##check if inverse has been calculated before
+  ##  If yes -> use chached data
+  if(!is.null(inverse)){
+    message("Using chaced data instead")
+    return(m)
+  }
+  
+  ##solve the inverse of the original matrix
+  originalMatrix <- inverse$getInverse()
+  m <- solve(originalMatrix, ...)
+  
+  ## set the cached matrix
+  inverse$setMatrix(m)
+
+  ##return the inversed matrix
+  m
+  
 }
